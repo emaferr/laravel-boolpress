ba29c3c92b623c0985c378284b4995fdd992083d
emaferr <ema.ferr@icloud.com>
2021-07-28 19:16:11 +0200

    Add category to existing record with tinker

--- database/migrations/2021_07_28_163741_add_category_id_to_articles_table.php
+++ database/migrations/2021_07_28_163741_add_category_id_to_articles_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use GuzzleHttp\Psr7\DroppingStream;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,7 +15,10 @@ class AddCategoryIdToArticlesTable extends Migration
     public function up()
     {
         Schema::table('articles', function (Blueprint $table) {
-            //
+            
+            $table->unsignedBigInteger('category_id')->nullable()->after('id');
+            $table->foreign('category_id')->references('id')->onDelete('set null');
+
         });
     }
 
@@ -26,7 +30,8 @@ class AddCategoryIdToArticlesTable extends Migration
     public function down()
     {
         Schema::table('articles', function (Blueprint $table) {
-            //
+            $table->dropForeign('articles_category_id_foreign');
+            $table->dropColumn('category_id');
         });
     }
 }
