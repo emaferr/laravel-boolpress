f9a5fb5b2006666ddd8cad16bfc47a82b24a96f5
emaferr <ema.ferr@icloud.com>
2021-07-29 17:19:29 +0200

    End one to many relationship

--- resources/views/guest/articles/show.blade.php
+++ resources/views/guest/articles/show.blade.php
@@ -2,16 +2,17 @@
 
 @section('content')
 
-<div class="container pb-5">
-    <h1>Single view</h1>
-    <div class="row pb-3">
-      <div class="col-4 pb-5">
-        <img class="img-thumbnail" src="{{{asset('storage/' . $article->image)}}" alt="">
-        <h3>{{$article->title}}</h3>
-        <p>{{$article->body}}</p>
-        <h5>{{$article->autor}}</h5>
-      </div>
+    <div class="container pb-5">
+        <h1>Single view</h1>
+        <div class="row pb-3">
+            <div class="col-4 pb-5">
+                <img class="img-thumbnail" src="{{ asset('storage/' . $article->image) }}" alt="">
+                <h3>{{ $article->title }}</h3>
+                <h5>Category: {{ $article->category ? $article->category->name : 'Uncategory' }}</h5>
+                <p>{{ $article->body }}</p>
+                <h5>{{ $article->autor }}</h5>
+            </div>
+        </div>
     </div>
-</div>
-    
+
 @endsection
